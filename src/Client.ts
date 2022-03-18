d373f208e3b28d8bd082c18464668051de77faca
Bluzzi <camilledugas.pro@gmail.com>
2022-03-18 15:50:47 +0100

    Client: set all intents

--- src/Client.ts
+++ src/Client.ts
@@ -4,7 +4,9 @@ import CommandListener from "./commands/CommandListener";
 import EventListener from "./events/EventListener";
 
 // Create client instance :
-export const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const allIntents = new Intents(32767);
+
+export const client = new Client({ intents: allIntents });
 
 // Start events and commands listeners :
 export const eventListener = new EventListener().load(client);
