f3e80c4d922f00791d3cdd848d4105d09025ead8
Bluzzi <camilledugas.pro@gmail.com>
2022-03-22 19:01:08 +0100

    Ready (event): fix the nickname changer for a guild

--- src/events/list/Ready.ts
+++ src/events/list/Ready.ts
@@ -1,15 +1,16 @@
 import { client } from "../../Client";
 import EventAbstract from "../EventAbstract";
+import { guildId } from "../../../resources/json/secret.json";
 
 export default class Ready extends EventAbstract {
     
     public name: string = "ready";
     public once = true;
 
-    public execute() : void {
-        client.user?.setUsername("The Tchoos");
+    public async execute() : Promise<void> {
+        (await client.guilds.fetch(guildId)).me?.setNickname("Tchoos");
         client.user?.setActivity({ type: "LISTENING", name: "your commands (do /)" })
 
-        console.log("[Tchoos BOT] Client is ready !");
+        console.log("[Tchoos BOT] Client is ready !"); // TODO : use log system
     }
 }
