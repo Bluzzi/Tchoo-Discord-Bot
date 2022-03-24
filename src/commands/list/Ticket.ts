a9e884c7e80dd41a8bed5396609566e5eb4d62a0
Bluzzi <camilledugas.pro@gmail.com>
2022-03-24 18:11:12 +0100

    Create Ticket command

---
+++ src/commands/list/Ticket.ts
@@ -0,0 +1,44 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction, MessageActionRow, MessageButton } from "discord.js";
+import CommandAbstract from "../CommandAbstract";
+import Embed from "../../utils/Embed";
+
+export default class Ticket extends CommandAbstract {
+
+    constructor(){
+        const command = new SlashCommandBuilder()
+            .setName("ticket")
+            .setDescription("Create a button to open tickets in this channel");
+
+        super(command);
+    }
+
+    public async execute(interaction: CommandInteraction) : Promise<void> {
+        if(!interaction.memberPermissions?.has("ADMINISTRATOR")){
+            interaction.reply({ 
+                embeds: [Embed.simple("You don't have the permission to do this command")], 
+                ephemeral: true 
+            });
+
+            return;
+        }
+
+        const embed = Embed.simple(
+            "This is the place to open a ticket for assistance. " +
+            "Please do not ping staff in tickets unless you have not " +
+            "gotten a response within 24 hours. Click the button below to open a ticket.", 
+            "Open a ticket"
+        );
+
+        const row = new MessageActionRow().addComponents(
+            new MessageButton()
+                .setCustomId("ticket-create")
+                .setLabel("Open a ticket")
+                .setStyle("PRIMARY")
+                .setEmoji("🎫")
+        )
+
+        interaction.channel?.send({ embeds: [embed], components: [row] });
+        interaction.reply({ embeds: [Embed.simple("The ticket opening message has been sent")], ephemeral: true });
+    }
+}
