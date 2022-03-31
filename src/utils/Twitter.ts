eeb525b72aa76ce1355e6ecaa7dc1220611f2610
Bluzzi <camilledugas.pro@gmail.com>
2022-03-31 16:18:17 +0200

    Create Twitter util file

---
+++ src/utils/Twitter.ts
@@ -0,0 +1,61 @@
+import fetch from "node-fetch";
+import { twitterToken } from "../../resources/json/secret.json";
+import { twitterAccount } from "../../resources/json/information.json";
+
+interface Tweet {
+    id: string;
+    text: string;
+}
+
+const authorization = {
+    authorization: "Bearer " + twitterToken
+}
+
+export default class Twitter {
+
+    private static instance: Twitter;
+
+    private accountId: string;
+
+    constructor(accountId: string){
+        if(Twitter.instance){
+            throw new Error("Use the static getInstance() method to get the unique instance of this class");
+        }
+
+        this.accountId = accountId;
+    }
+
+    public static async getInstance() : Promise<Twitter> {
+        if(!Twitter.instance){
+            const response = await fetch("https://api.twitter.com/2/users/by/username/" + twitterAccount, {
+                headers: authorization
+            });
+            const { data } = await response.json();
+
+            Twitter.instance = new Twitter(data.id);
+        }
+
+        return Twitter.instance;
+    }
+
+    /**
+     * @returns the last 10 tweets
+     */
+    public async getTweets() : Promise<Tweet[]> {
+        const response = await fetch("https://api.twitter.com/2/users/" + this.accountId + "/tweets?exclude=retweets,replies", {
+            headers: authorization
+        });
+        const { data } = await response.json();
+
+        return data;
+    }
+
+    public async getFollowersCount() : Promise<number> {
+        const response = await fetch("https://api.twitter.com/2/users/by/username/TheTchoos?user.fields=public_metrics", {
+            headers: authorization
+        });
+        const { data } = await response.json();
+        
+        return data["public_metrics"]["followers_count"] ?? 0;
+    }
+}
